8ad6e50c1240f326a6fa6d07ff331d168fa374cd
Catalin Anechei <catalin.anechei@orange.com>
2017-09-19 16:51:31 +0300

    TRy

--- app/index.php
+++ app/index.php
@@ -13,5 +13,4 @@
  * @since         CakePHP(tm) v 0.10.0.1076
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
-apache_setenv("KeepAlive", "Off");
 require 'webroot' . DIRECTORY_SEPARATOR . 'index.php';
